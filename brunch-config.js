35f77f3df75bdca4c81bc37fc0440cc4c1ddf9b3
Jérémie DEGHAYE <jeremiedeghaye@macbook-air-de-jeremie.home>
2019-02-26 11:26:43 +0100

    config brunch modif dependencies

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,47 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+  javascripts: {
+    joinTo: {
+      'js/app.js': /^app/,
+      'js/vendor.js': /^node_modules/,
+    }
+  },
+  stylesheets: {
+    joinTo: {
+      'css/app.css': /^app/,
+      'css/vendor.css': /^node_modules/,
+    }
+  }
+};
+
+exports.plugins = {
+  browserSync: {
+    files: ['*']
+  },
+  copycat: {
+    'fonts': ['node_modules/font-awesome/fonts']
+  }
+};
+
+exports.watcher = {
+  usePolling: true,
+  awaitWriteFinish: true
+};
+
+exports.npm = {
+  styles: {
+    'font-awesome': ['css/font-awesome.css'],
+    'normalize.css': ['normalize.css']
+  },
+  globals: {
+    '$': 'jquery',
+    'jQuery': 'jquery',
+    'scrollex': 'jquery.scrollex'
+  }
+};
+
+exports.modules = {
+  autoRequire: {
+    'js/app.js': ['initialize']
+  }
 };
